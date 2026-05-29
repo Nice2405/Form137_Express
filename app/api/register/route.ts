c7177679a4e7200db627d1087f7fb37958f11d63
Nice2405 <fernandezmavenice@gmail.com>
2026-05-29 22:51:40 +0800

    main

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,18 +1,33 @@
 import { NextResponse } from "next/server";
+import { pool } from "@/lib/db";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    console.log(body);
+    const { email, password, role } = body;
+
+    await pool.query(
+      `
+      INSERT INTO users (email, password, role)
+      VALUES ($1, $2, $3)
+      `,
+      [email, password, role]
+    );
 
     return NextResponse.json({
       success: true,
       message: "Registered successfully",
     });
+
   } catch (error) {
+    console.log(error);
+
     return NextResponse.json(
-      { success: false, error: "Server error" },
+      {
+        success: false,
+        error: "Server error",
+      },
       { status: 500 }
     );
   }
