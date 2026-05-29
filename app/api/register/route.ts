39ad4e780214bed691c734ee4728cfdbd63cd4f0
Nice2405 <fernandezmavenice@gmail.com>
2026-05-30 00:41:20 +0800

    main

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,22 +3,31 @@ import { pool } from "@/lib/db";
 
 export async function POST(req: Request) {
   try {
+    console.log("REGISTER ROUTE HIT");
+
     const body = await req.json();
 
     console.log("BODY:", body);
 
-    const { email, password, role } = body;
+    const {
+      fullName,
+      email,
+      password,
+      role,
+      program,
+    } = body;
 
     const result = await pool.query(
       `
-      INSERT INTO users (email, password, role)
-      VALUES ($1, $2, $3)
+      INSERT INTO users
+      (full_name, email, password, role, program)
+      VALUES ($1, $2, $3, $4, $5)
       RETURNING *
       `,
-      [email, password, role]
+      [fullName, email, password, role, program]
     );
 
-    console.log("INSERT SUCCESS:", result.rows);
+    console.log("USER INSERTED:", result.rows);
 
     return NextResponse.json({
       success: true,
