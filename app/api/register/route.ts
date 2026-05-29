a7f32a20e87b022e204b764c08ea0da9937dda77
Nice2405 <fernandezmavenice@gmail.com>
2026-05-29 22:59:12 +0800

    main

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
     });
 
   } catch (error) {
-    console.log(error);
+    console.error("REGISTER ERROR:", error);
 
     return NextResponse.json(
       {
