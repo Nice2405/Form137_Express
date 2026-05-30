8f2348440412b0887117a17e5512617bd54f6081
Nice2405 <fernandezmavenice@gmail.com>
2026-05-30 09:06:14 +0800

    main

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,5 @@
 import { Pool } from "pg";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing");
-}
-
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
